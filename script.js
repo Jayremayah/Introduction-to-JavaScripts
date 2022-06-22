11d762f98c086fa9fad6b5e5eefb1a5f389e5afe
Conrad <jude@imagefactor.com.ng>
2022-06-22 16:50:00 +0100

    update

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let height = '174';
+// let height = '174cm';
 
-let name = "Okoroafor Jerry";
+// let myname = "Okoroafor Jerry";
 
-let country = "Nigeria";
-console.log
+// let country = "Nigeria";
+// console.log(height, myname, country)
 
+let foodForTheWeek = ['yam', 'bread', 'garri', 'tamba',]
+
+console.table(foodForTheWeek)
+
+for (let i = 0; i <5; i += 1) {
+    console.log(i)
+}
